1d06dbe8c7d6e8abd2c9c169f48eef4cc29c1add
Wendy Hsu <wndyhsu@gmail.com>
2014-04-29 20:34:00 -0500

    working on game play page

--- web/app/assets/javascripts/game.js
+++ web/app/assets/javascripts/game.js
@@ -1,8 +1,14 @@
 
-
- $(document).ready(function(){ 
+ $(document).ready(function(){
 $('#reg').on('click',function(){
   $('.newform').slideDown('slow')
+
+
+$('.roll').on('click', function(){
+if ($('li').html().match(/^paws$/)) {
+  $('.roll').append("<img src='blast.png'/>")
+}
+});
 });
 });
 
