3016a0f45b926e303f2b469000d1443406ddb586
Wendy Hsu <wndyhsu@gmail.com>
2014-05-03 11:37:49 -0500

    working on sign up usecase

--- web/app/controllers/games_controller.rb
+++ web/app/controllers/games_controller.rb
@@ -10,12 +10,12 @@ class GamesController < ApplicationController
         players.delete(player)
       end
     end
-    
+
      @result = RabbitDice::CreateGame.run(:players => params[:players])
      if @result.success?
       flash[:success] = "Game has been created!"
       redirect_to "/games/#{@result.game.id}"
-    else 
+    else
       @error = @result.error
       # flash[:error] = "#{@error}"
       render 'new'
@@ -28,31 +28,39 @@ class GamesController < ApplicationController
     @current_player = @game.current_player
     @game_winner = @game.winner
     @last_turn = @game.turns.last
+    @results = @game.turns.rolls.last.results
     # @dice_result = @game.turns.last.rolls.last.results
     @wounds = @last_turn.wound_count
     @score = @game.score_for(@current_player)
 
+
     flash[:success]
+
+    respond_to do |format|
+      format.html
+      format.json {render :json => {game: @game, players: @players, current_player: @current_player, wounds: @wonuds, score: @score, :results: @results}}
+    end
+
   end
 
   def winner
     @game = RabbitDice.db.get_game(params[:id].to_i)
-    @winner = @game.winner 
+    @winner = @game.winner
     # flash[:success]
 
   end
-  
+
 
   def update
     move = params[:move]
-    @result = RabbitDice::PlayMove.run(:game_id => params[:id].to_i, :move => params[:move]) 
+    @result = RabbitDice::PlayMove.run(:game_id => params[:id].to_i, :move => params[:move])
     @game = RabbitDice.db.get_game(params[:id].to_i)
-    if @game.winner != nil 
+    if @game.winner != nil
       redirect_to "/games/winner/#{@game.id}"
-    elsif @result.success? 
+    elsif @result.success?
       # flash[:success] = 'You moved~!'
       redirect_to "/games/#{@result.game.id}"
-    else 
+    else
       @error = @result.error
       render 'new'
     end
